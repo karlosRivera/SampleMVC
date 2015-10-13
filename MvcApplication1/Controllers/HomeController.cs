7d6bfc32e56321beea0516f4a6e74cb68037b063
tridip-bba <tridip@bba-reman.com>
2015-10-13 20:59:15 +0530

    checked

--- MvcApplication1/Controllers/HomeController.cs
+++ MvcApplication1/Controllers/HomeController.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.ComponentModel;
+using MvcApplication1.Models;
 
 namespace MvcApplication1.Controllers
 {
     public class HomeController : Controller
     {
+        
         public ActionResult Index()
         {
 
@@ -21,6 +23,42 @@ namespace MvcApplication1.Controllers
             return View(emp);
         }
 
+        public ActionResult MyCollection()
+        {
+            var oVm = new MyViewModel
+            {
+                CustomerName = "pop",
+                hobbies = new List<Hobbies>
+                {
+                    new Hobbies {ID=1,Name="Foot ball",IsSelected=false},
+                    new Hobbies {ID=2,Name="Table Tennis",IsSelected=false},
+                    new Hobbies {ID=3,Name="Cricket",IsSelected=false},
+                    new Hobbies {ID=4,Name="Web Surfing",IsSelected=false},
+                    new Hobbies {ID=5,Name="Listening Music",IsSelected=false}
+                },
+
+                movies = new List<Movies>
+                {
+                    new Movies {ID=1,Name="Blood Sports",IsSelected=false},
+                    new Movies {ID=2,Name="Good Kill",IsSelected=false},
+                    new Movies {ID=3,Name="Avtar",IsSelected=false},
+                    new Movies {ID=4,Name="Lost Africa",IsSelected=false},
+                    new Movies {ID=5,Name="The Hell",IsSelected=false}
+                }
+            };
+
+            return View(oVm);
+        }
+
+        [HttpPost]
+        public ActionResult MyCollection(MyViewModel oVm, string CustomerName)
+        {
+            ViewBag.Message = "Success";
+            return View(oVm);
+        }
+
+
+        [Authorize]
         public ActionResult About()
         {
             ViewBag.Message = "Your app description page.";
@@ -34,8 +72,39 @@ namespace MvcApplication1.Controllers
 
             return View();
         }
+
+        public ActionResult Form1()
+        {
+            Register rgs = new Register();
+            return View(rgs);
+        }
+
+        [HttpPost]
+        public ActionResult Form1(Register rgs)
+        {
+            if (rgs.FName.Length<10)
+            {
+                ViewBag.Message = "FAIL";
+            }
+            else
+            {
+                ViewBag.Message = "SUCCESS";
+            }
+            return View(rgs);
+        }
+
+        public string SayHi()
+        {
+            return "hi from 'SayHi'.";
+        }  
     }
 
+    public class Register
+    {
+        public string FName { get; set; }
+        public string LName { get; set; }
+        public bool IsApproved { get; set; }
+    }
     public class Employee
     {
         public int Id { get; set; }
---
+++ MvcApplication1/Models/MyModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcApplication1.Models
+{
+    public class MyViewModel
+    {
+        public string CustomerName { get; set; }
+
+        //[UIHint("Movies1")]
+        public List<Movies> movies { get; set; }
+
+        //[UIHint("Hobbies1")]
+        public List<Hobbies> hobbies { get; set; }
+    }
+
+    public class Movies
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public bool IsSelected { get; set; }
+    }
+
+    public class Hobbies
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public bool IsSelected { get; set; }
+    }
+}
